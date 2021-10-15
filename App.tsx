7abecf47c63255ce7135e313226760d2ca8115c3
Niko Sward <nicholascsward@gmail.com>
2021-10-14 23:18:47 -0500

    working ios example

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@
 
 import React, {useState} from 'react';
 import {
+  Image,
+  NativeModules,
   SafeAreaView,
   ScrollView,
   StyleSheet,
@@ -18,11 +20,8 @@ import {
   View,
 } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
-
-import TesseractOcr, {
-  LANG_ENGLISH,
-  useEventListener,
-} from 'react-native-tesseract-ocr';
+import SampleImage from './sample_image.jpg';
+import Receipt from './Receipt.jpg';
 
 const DEFAULT_HEIGHT = 500;
 const DEFAULT_WITH = 600;
@@ -32,6 +31,14 @@ const defaultPickerOptions = {
   width: DEFAULT_WITH,
 };
 
+type coords = {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+};
+
+//ex component in ts
 const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
@@ -43,18 +50,35 @@ const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
   const [imgSrc, setImgSrc] = useState<any>(null);
+  const {OCR} = NativeModules;
 
-  const recognizeTextFromImage = async path => {
+  //loads image from picker to state and passes to tesseract
+  const recognizeFromPicker = async (options = defaultPickerOptions) => {
+    try {
+      const image: any = await ImagePicker.openPicker(options);
+      console.log(image);
+      setImgSrc({uri: image.path});
+      await recognizeTextFromImage(image.sourceURL, image.cropRect);
+    } catch (err: any) {
+      if (err.message !== 'User cancelled image selection') {
+        console.error(err);
+      }
+    }
+  };
+
+  //scans image for text
+  const recognizeTextFromImage = async (imagePath: any, coords: coords) => {
     setIsLoading(true);
 
     try {
-      const tesseractOptions = {};
-      const recognizedText = await TesseractOcr.recognize(
-        path,
-        LANG_ENGLISH,
-        tesseractOptions,
-      );
-      setText(recognizedText);
+      // coords = {x: 0, y: 0, width: 600, height: 500}; //testing only
+      // imagePath = Image.resolveAssetSource(SampleImage).uri; // testing only
+
+      // coords = {height: 107, width: 129, x: 458, y: 398}; //testing only
+      // imagePath = Image.resolveAssetSource(Receipt).uri; // testing only
+      const recognizedText = await OCR.scanForText(imagePath, coords);
+
+      setText(recognizedText || 'Could not find text');
     } catch (err) {
       console.error(err);
       setText('');
@@ -62,18 +86,6 @@ const App = () => {
 
     setIsLoading(false);
     setProgress(0);
-  };
-
-  const recognizeFromPicker = async (options = defaultPickerOptions) => {
-    try {
-      const image = await ImagePicker.openPicker(options);
-      setImgSrc({uri: image.path});
-      await recognizeTextFromImage(image.path);
-    } catch (err: any) {
-      if (err.message !== 'User cancelled image selection') {
-        console.error(err);
-      }
-    }
   };
 
   return (
@@ -92,6 +104,11 @@ const App = () => {
         </TouchableOpacity>
         <Text>{text}</Text>
         {isLoading === true && <Text>loading: {progress}%</Text>}
+        {imgSrc && (
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={imgSrc} />
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -111,6 +128,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginVertical: 15,
+    height: DEFAULT_HEIGHT / 2.5,
+    width: DEFAULT_WITH / 2.5,
   },
 });
 
